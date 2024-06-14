7dac8a5f8e0f5b367a082d98db130a8dba8ff113
Suunic <ricardoavelal@gmail.com>
2024-06-14 16:47:30 +0100

    -Criados os botões para 1 jogador, 2 jogadores e saida da aplicação tal como as páginas de para os botões de 1 jogador e 2 jogadores com botões para voltar para a página inicial
    -Adicionadas as dependencias necessárias para mudança entre páginas
    -Adicionado aos Drawables as diferentes faces do dado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -59,6 +58,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    // Jetpack Compose Navigation
+    implementation("androidx.navigation:navigation-compose:2.5.3")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -67,3 +68,4 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
+
--- app/src/main/java/com/example/projetoandroidstdricardosousa/MainActivity.kt
+++ app/src/main/java/com/example/projetoandroidstdricardosousa/MainActivity.kt
@@ -1,47 +1,129 @@
+@file:Suppress("CAST_NEVER_SUCCEEDS")
+
 package com.example.projetoandroidstdricardosousa
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.projetoandroidstdricardosousa.ui.theme.ProjetoAndroidStdRicardoSousaTheme
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.projetoandroidstdricardosousa.ui.theme.ProjectGameTheme
+import kotlin.system.exitProcess
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            ProjetoAndroidStdRicardoSousaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            ProjectGameTheme {
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "main") {
+        composable("main") {
+            MainScreen(
+                navigateToScreen1 = { navController.navigate("screen1") },
+                navigateToScreen2 = { navController.navigate("screen2") },
+                exitApp = { (it as ComponentActivity).finishAffinity() }
+            )
+        }
+        composable("screen1") {
+            Screen1(navigateBack = { navController.popBackStack() })
+        }
+        composable("screen2") {
+            Screen2(navigateBack = { navController.popBackStack() })
+        }
+
+
+    }
+}
+
+@Composable
+fun MainScreen(navigateToScreen1: () -> Unit, navigateToScreen2: () -> Unit, exitApp:() -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = navigateToScreen1) {
+            Text("1 Player")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = navigateToScreen2) {
+            Text("2 Players")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = exitApp) {
+            Text("Exit")
+        }
+    }
+}
+
+@Composable
+fun Screen1(navigateBack: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("This is Screen 1")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = navigateBack) {
+            Text("Back to Main Screen")
+        }
+    }
+}
+
+@Composable
+fun Screen2(navigateBack: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("This is Screen 2")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = navigateBack) {
+            Text("Back to Main Screen")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ProjetoAndroidStdRicardoSousaTheme {
-        Greeting("Android")
+fun PreviewMainScreen() {
+    ProjectGameTheme {
+        MainScreen(navigateToScreen1 = {}, navigateToScreen2 = {}, exitApp = {})
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewScreen1() {
+    ProjectGameTheme {
+        Screen1(navigateBack = {})
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewScreen2() {
+    ProjectGameTheme {
+        Screen2(navigateBack = {})
+    }
+}
+
