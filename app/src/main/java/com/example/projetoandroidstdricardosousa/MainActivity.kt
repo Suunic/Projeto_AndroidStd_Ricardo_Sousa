fedbe12441c6661cbac4e408f636403efdaa73bc
Suunic <ricardoavelal@gmail.com>
2024-06-20 18:45:08 +0100

    -Adicionado um novo botão :Options no menu incial -Criado um novo screen para as Options -Criada nova função: NumberPicker -Modificada a função DiceSix aceitando novos parametros para 2 jogadores ou apenas 1 -Criada função para o dado de 20 faces DiceTwenty -Modificadas os valores da variável Options -Aplicado ao Screen2(2 players screen) as mesmas funcionalidades que no Screen1 (1 player screen) de chamar as funções dos dados -Adicionado a funcionalidade de parar a contagem para o botão de Stop e calcular quem ganhou o jogo

--- app/src/main/java/com/example/projetoandroidstdricardosousa/MainActivity.kt
+++ app/src/main/java/com/example/projetoandroidstdricardosousa/MainActivity.kt
@@ -36,7 +36,8 @@ import com.example.projetoandroidstdricardosousa.ui.theme.ProjectGameTheme
 import kotlin.system.exitProcess
 
 
-var Option: Int = 2
+var Option: Int = 6
+var Value:  Int = 21
 
 
 class MainActivity : ComponentActivity() {
@@ -60,6 +61,7 @@ fun MyApp() {
             MainScreen(
                 navigateToScreen1 = { navController.navigate("screen1") },
                 navigateToScreen2 = { navController.navigate("screen2") },
+                navigateToScreen3 = { navController.navigate("screen3") },
                 exitApp = { exitApplication() }
             )
         }
@@ -68,6 +70,9 @@ fun MyApp() {
         }
         composable("screen2") {
             Screen2(navigateBack = { navController.popBackStack() })
+        }
+        composable("screen3") {
+            Screen3(navigateBack = { navController.popBackStack() })
         }
     }
 }
@@ -80,6 +85,9 @@ fun exitApplication() {
 fun MainScreen(
     navigateToScreen1: () -> Unit,
     navigateToScreen2: () -> Unit,
+    navigateToScreen3: () -> Unit,
+
+
     exitApp: () -> Unit
 ) {
     Column(
@@ -93,6 +101,10 @@ fun MainScreen(
         Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = navigateToScreen2) {
             Text("2 Players")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = navigateToScreen3) {
+            Text("Options")
         }
         Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = exitApp) {
@@ -125,9 +137,9 @@ fun Screen1(navigateBack: () -> Unit) {
                     .width(IntrinsicSize.Max)
             ) {
 
-                DropdownMenuItem(text = { Text("D4" ) }, onClick = { Option=1; Log.d("TAG", "Option changed to $Option") })
-                DropdownMenuItem(text = { Text("D6" ) }, onClick = { Option=2; Log.d("TAG", "Option changed to $Option") })
-                DropdownMenuItem(text = { Text("D20") }, onClick = { Option=3; Log.d("TAG", "Option changed to $Option") })
+                DropdownMenuItem(text = { Text("D4" ) }, onClick = { Option=4; Log.d("TAG", "Option changed to $Option") })
+                DropdownMenuItem(text = { Text("D6" ) }, onClick = { Option=6; Log.d("TAG", "Option changed to $Option") })
+                DropdownMenuItem(text = { Text("D20") }, onClick = { Option=20; Log.d("TAG", "Option changed to $Option") })
 
             }
         }
@@ -136,14 +148,14 @@ fun Screen1(navigateBack: () -> Unit) {
     }
     Spacer(modifier = Modifier.padding(end = 16.dp))
     when (Option) {
-        1 -> {
+        4 -> {
             // diceFour(1, player1)
         }
-        2 -> {
-            DiceSix(1, player1)
+        6 -> {
+            DiceSix(1, Value)
         }
-        3 -> {
-            // diceTwenty(1, player1)
+        20 -> {
+            DiceTwenty(2, Value )
         }
     }
 
@@ -158,6 +170,7 @@ fun Screen1(navigateBack: () -> Unit) {
         Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = navigateBack) {
             Text("Back to Main Screen")
+
         }
         Spacer(modifier = Modifier.height(40.dp))
     }
@@ -187,9 +200,9 @@ fun Screen2(navigateBack: () -> Unit) {
                     .width(IntrinsicSize.Max)
             ) {
 
-                DropdownMenuItem(text = { Text("D4" ) }, onClick = { Option=1; Log.d("TAG", "Option changed to $Option") })
-                DropdownMenuItem(text = { Text("D6" ) }, onClick = { Option=2; Log.d("TAG", "Option changed to $Option") })
-                DropdownMenuItem(text = { Text("D20") }, onClick = { Option=3; Log.d("TAG", "Option changed to $Option") })
+                DropdownMenuItem(text = { Text("D4" ) }, onClick = { Option=4; Log.d("TAG", "Option changed to $Option") })
+                DropdownMenuItem(text = { Text("D6" ) }, onClick = { Option=6; Log.d("TAG", "Option changed to $Option") })
+                DropdownMenuItem(text = { Text("D20") }, onClick = { Option=20; Log.d("TAG", "Option changed to $Option") })
 
             }
         }
@@ -198,14 +211,14 @@ fun Screen2(navigateBack: () -> Unit) {
     }
     Spacer(modifier = Modifier.padding(end = 16.dp))
     when (Option) {
-        1 -> {
+        4 -> {
             // diceFour(1, player1)
         }
-        2 -> {
-            DiceSix(2, player1)
+        6 -> {
+            DiceSix(2, Value)
         }
-        3 -> {
-            // diceTwenty(1, player1)
+        20 -> {
+            DiceTwenty(2, Value )
         }
     }
 
@@ -225,11 +238,61 @@ fun Screen2(navigateBack: () -> Unit) {
     }
 }
 
+@Composable
+fun Screen3(navigateBack: () -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+    var customValue by remember { mutableStateOf(Value) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Box {
+            Button(onClick = { expanded = true }) {
+                Text("D$Option ")
+            }
+
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.width(IntrinsicSize.Max)
+            ) {
+                DropdownMenuItem(text = { Text("D4" ) }, onClick = { Option=4; Log.d("TAG", "Option changed to $Option") })
+                DropdownMenuItem(text = { Text("D6" ) }, onClick = { Option=6; Log.d("TAG", "Option changed to $Option") })
+                DropdownMenuItem(text = { Text("D20") }, onClick = { Option=20; Log.d("TAG", "Option changed to $Option") })
+
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text("Select the desired value to play with", fontSize = 20.sp)
+        Spacer(modifier = Modifier.height(8.dp))
+        NumberPicker(
+            value = customValue,
+            onValueChange = { customValue = it },
+            range = 1..100
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+                navigateBack()
+                Value = customValue
+            }) {
+            Text("Back to Main Screen")
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewMainScreen() {
     ProjectGameTheme {
-        MainScreen(navigateToScreen1 = {}, navigateToScreen2 = {}, exitApp = {})
+        MainScreen(navigateToScreen1 = {}, navigateToScreen2 = {}, navigateToScreen3 = {}, exitApp = {})
     }
 }
 
@@ -250,7 +313,8 @@ fun PreviewScreen2() {
 }
 
 @Composable
-fun DiceSix( playerSelect: Int, player: Player) {
+fun DiceSix( playerSelect: Int, value: Int) {
+    var checkValue by remember { mutableStateOf(false) }
     var result by remember { mutableStateOf(1) }
     var textRoll by remember { mutableStateOf("Player 1") }
     var currentPlayer by remember { mutableStateOf(player1) }
@@ -264,6 +328,7 @@ fun DiceSix( playerSelect: Int, player: Player) {
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
+    Spacer(modifier = Modifier.height(20.dp))
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -276,22 +341,27 @@ fun DiceSix( playerSelect: Int, player: Player) {
 
         Button(
             onClick = {
-                result = (1..6).random()
-                if (playerSelect == 1) {
-                    textRoll = "Player 1"
-                    getDiceValue(player = player1, roll = result)
-                    result = (1..6).random()
-                    getDiceValue(player = player2, roll = result)
 
-                } else {
-                    textRoll = if (currentPlayer == player1) "Player 1" else "Player 2"
-                    getDiceValue(player = currentPlayer, roll = result)
-                    if (currentPlayer == player1) {
-                        currentPlayer = player2
+                if(!checkValue){
+
+                    result = (1..6).random()
+                    if (playerSelect == 1) {
+                        textRoll = "Player 1"
+                        getDiceValue(player = player1, roll = result)
+                        result = (1..6).random()
+                        getDiceValue(player = player2, roll = result)
+
 
                     } else {
-                        currentPlayer = player1
+                        textRoll = if (currentPlayer == player1) "Player 1" else "Player 2"
+                        getDiceValue(player = currentPlayer, roll = result)
+                        if (currentPlayer == player1) {
+                            currentPlayer = player2
 
+                        } else {
+                            currentPlayer = player1
+
+                        }
                     }
                 }
             },
@@ -302,7 +372,105 @@ fun DiceSix( playerSelect: Int, player: Player) {
         Spacer(modifier = Modifier.height(16.dp))
         Button(
             onClick = {
-                //Stop
+                checkValue = true
+                val player1Diff = Math.abs(value - player1.score)
+                val player2Diff = Math.abs(value - player2.score)
+                when {
+                    player1Diff < player2Diff -> Log.d("TAG", "Player 1 Wins")
+                    player1Diff == player2Diff -> Log.d("TAG", "Draw")
+                    else -> Log.d("TAG", "Player 2 Wins")
+            }
+            }
+
+        ){
+            Text(text = "Stop", fontSize = 24.sp)
+        }
+    }
+}
+
+@Composable
+fun DiceTwenty( playerSelect: Int, value: Int) {
+    var checkValue by remember { mutableStateOf(false) }
+    var result by remember { mutableStateOf(1) }
+    var textRoll by remember { mutableStateOf("Player 1") }
+    var currentPlayer by remember { mutableStateOf(player1) }
+
+    val imageResource = when (result) {
+
+        1 -> R.drawable.d20_1
+        2 -> R.drawable.d20_2
+        3 -> R.drawable.d20_3
+        4 -> R.drawable.d20_4
+        5 -> R.drawable.d20_5
+        6 -> R.drawable.d20_6
+        7 -> R.drawable.d20_7
+        8 -> R.drawable.d20_8
+        9 -> R.drawable.d20_9
+        10 -> R.drawable.d20_10
+        11 -> R.drawable.d20_11
+        12 -> R.drawable.d20_12
+        13 -> R.drawable.d20_13
+        14 -> R.drawable.d20_14
+        15 -> R.drawable.d20_15
+        16 -> R.drawable.d20_16
+        17 -> R.drawable.d20_17
+        18 -> R.drawable.d20_18
+        19 -> R.drawable.d20_19
+        else -> R.drawable.d20_20
+
+    }
+    Spacer(modifier = Modifier.height(20.dp))
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(imageResource),
+            contentDescription = result.toString()
+        )
+
+        Button(
+            onClick = {
+
+                if(!checkValue){
+
+                    result = (1..20).random()
+                    if (playerSelect == 1) {
+                        textRoll = "Player 1"
+                        getDiceValue(player = player1, roll = result)
+                        result = (1..20).random()
+                        getDiceValue(player = player2, roll = result)
+
+
+                    } else {
+                        textRoll = if (currentPlayer == player1) "Player 1" else "Player 2"
+                        getDiceValue(player = currentPlayer, roll = result)
+                        if (currentPlayer == player1) {
+                            currentPlayer = player2
+
+                        } else {
+                            currentPlayer = player1
+
+                        }
+                    }
+                }
+            },
+
+            ) {
+            Text("Roll($textRoll)", fontSize = 24.sp)
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                checkValue = true
+                val player1Diff = Math.abs(value - player1.score)
+                val player2Diff = Math.abs(value - player2.score)
+                when {
+                    player1Diff < player2Diff -> Log.d("TAG", "Player 1 Wins")
+                    player1Diff == player2Diff -> Log.d("TAG", "Draw")
+                    else -> Log.d("TAG", "Player 2 Wins")
+                }
             }
 
         ){
@@ -320,5 +488,22 @@ val player2 = Player()
 
 fun getDiceValue(player: Player, roll: Int) {
     player.score += roll
+}
+
+@Composable
+fun NumberPicker(value: Int, onValueChange: (Int) -> Unit, range: IntRange) {
+    var currentValue by remember { mutableStateOf(value) }
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(onClick = { if (currentValue > range.first) currentValue--; onValueChange(currentValue) }) {
+            Text("-")
+        }
+
+        Text(currentValue.toString(), fontSize = 24.sp)
+
+        Button(onClick = { if (currentValue < range.last) currentValue++; onValueChange(currentValue) }) {
+            Text("+")
+        }
+    }
 }
 
